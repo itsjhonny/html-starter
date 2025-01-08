a5f84462ba5269df048c7be05c44a25993939cd3
itsjhonny <jowhhn@@gmail.com>
2025-01-07 21:19:34 -0300

    first commit

--- middleware.js
+++ middleware.js
@@ -2,40 +2,19 @@
 
 import { NextResponse } from "@vercel/edge";
 
-export default function middleware(req) {
-  const { pathname } = req.nextUrl;
+export default async function middleware(request) {
+  const url = new URL(request.url);
 
-  // Mapeamento dos códigos de idioma suportados
-  const supportedLanguages = ["pt_br", "en_us", "es_es"];
-
-  // Extrair o primeiro segmento do caminho para verificar o idioma
-  const pathSegments = pathname
-    .split("/")
-    .filter((segment) => segment.length > 0);
-  const currentLang = pathSegments[0];
-
-  // Se o idioma não estiver presente ou não for suportado, redirecionar para 'en_us'
-  if (!supportedLanguages.includes(currentLang)) {
-    const url = req.nextUrl.clone();
-    url.pathname = `/en_us${pathname}`;
-    return NextResponse.redirect(url);
+  if (url.pathname === "/") {
+    // Redirecionamento permanente para '/en_us/'
+    return NextResponse.redirect(new URL("/en_us/", request.url), 308);
   }
 
-  // Adicionar cabeçalhos de segurança
-  const response = NextResponse.next();
-  response.headers.set("Referrer-Policy", "origin-when-cross-origin");
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("X-DNS-Prefetch-Control", "on");
-  response.headers.set(
-    "Strict-Transport-Security",
-    "max-age=31536000; includeSubDomains; preload"
-  );
-
-  return response;
+  // Continua o fluxo normal da requisição
+  return NextResponse.next();
 }
 
-// Definir quais caminhos devem usar o middleware
+// Definição de quais caminhos devem usar o middleware
 export const config = {
   matcher: "/:path*",
 };
